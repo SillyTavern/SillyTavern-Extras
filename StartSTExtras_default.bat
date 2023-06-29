e40a791b749c400290c482907a1b8afea5336e60
coffeebrown0 <136328295+coffeebrown0@users.noreply.github.com>
2023-06-30 03:11:31 +0400

    added comments to default startExtras.bat

--- StartSTExtras_default.bat
+++ StartSTExtras_default.bat
@@ -1,1 +1,3 @@
 python server.py --enable-modules=caption,summarize,classify 
+
+//if you want to enable other modules, simply make a copy of this file and modify the arguments above. Try to avoid mofidying this file itself, to avoid overwriting it when updating ST Extras in the future. Refer to https://docs.sillytavern.app/extras/installation/#running-extras-after-install for more information.
