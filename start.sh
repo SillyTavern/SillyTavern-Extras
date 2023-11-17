f88ff360c98f5cb92eae7f2d29dd54b3e6e8e748
deffcolony <61471128+deffcolony@users.noreply.github.com>
2023-11-17 17:07:12 +0100

    changes env to extras + flag updates

--- start.sh
+++ start.sh
@@ -3,15 +3,17 @@
 # Environment Variables
 miniconda_path="$HOME/miniconda"
 
-# Activate the Conda environment (sillytavernextras)
+# Activate the Conda environment (extras)
 source $miniconda_path/etc/profile.d/conda.sh
 conda config --set auto_activate_base false
 conda init bash
-conda activate sillytavernextras
+conda activate extras
 
 # Start the Python server for SillyTavern-extras. You can modify the flags below
-python server.py --coqui-gpu --rvc-save-file --cuda-device=0 --max-content-length=1000 --enable-modules=talkinghead,chromadb,caption,summarize,rvc,coqui-tts
-read -p 'PRESS ENTER TO CLOSE'
+python server.py --rvc-save-file --cuda-device=0 --max-content-length=1000 --enable-modules=summarize,rvc,
+read -p 'PRESS ANY KEY TO CLOSE'
+
+# https://docs.sillytavern.app/extras/installation/#decide-which-module-to-use
 
 # Coqui --enable-modules=coqui-tts --coqui-gpu --cuda-device=0
 # RVC --enable-modules=rvc --rvc-save-file --max-content-length=1000
